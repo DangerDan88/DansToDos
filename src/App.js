39ea957d4a8781d61989de327266e67b2d9e2c4e
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-03-17 16:32:03 -0600

    tryna make a to do list without hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import List from "./List";
+export default class App extends Component() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toDo: "",
+      items: []
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  onChange = event => {
+    this.setState({
+      toDo: event.target.value
+    });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+
+    this.setState({
+      toDo: "",
+      items: [...this.state.items, this.state.toDo]
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input value={this.state.toDo} onChange={this.onChange} />
+          <button>Add ToDo:</button>
+        </form>
+        <List items={this.state.items} />
+      </div>
+    );
+  }
 }
-
-export default App;
